30e7e50a04276d55aa9c45a5554a8546752a3b4f
LOGESH S <124980896+S-LOGESH-18@users.noreply.github.com>
2025-03-28 15:30:11 +0530

    Add files via upload

---
+++ problem1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NavBar from './Components/nav';
+import DataTable from './Components/Table';
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <DataTable/>
+    </>
+  );
+}
+
+export default App;
